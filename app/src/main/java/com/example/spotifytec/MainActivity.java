ce2d64e21b73d27c63b9fdfbb3ecd88cb339718c
chaos20 <33138600+chaos20@users.noreply.github.com>
2019-02-23 19:05:20 -0600

    Primer upload

---
+++ app/src/main/java/com/example/spotifytec/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.spotifytec;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "6bc2ccb771ff4ca2960d9c6c9a0f46be";
+    private static final String REDIRECT_URI = "testschema://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:3Lkk00YCbe4izdDyaeobSv");
+    }
+}
+
+//https://open.spotify.com/playlist/3Lkk00YCbe4izdDyaeobSv
